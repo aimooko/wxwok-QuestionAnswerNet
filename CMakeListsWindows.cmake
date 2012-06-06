3e5d9f80c11ba26294d63f2d7d9a60bba0e25413
Xuewen Wang <xuewen.ok@gmail.com>
2012-06-06 09:16:11 +0800

    Wed Jun  6 09:16:11 CST 2012

--- CMakeListsWindows.cmake
+++ CMakeListsWindows.cmake
@@ -2,4 +2,4 @@ set(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH}
     /media/Large/Develop/boost_1_47
     /media/Large/Develop/pthread/Pre-built.2)
 
-Message(FATAL_ERROR "this is included from MingW")
+include (CMakeLists.txt)
